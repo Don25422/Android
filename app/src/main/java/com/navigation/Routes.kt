4d3335158fe12b36ab8e3d89bdd94bf040c423d7
Don25422 <danglaskimathi5@gmail.com>
2025-04-04 16:37:55 +0300

    first commit

---
+++ app/src/main/java/com/navigation/Routes.kt
@@ -0,0 +1,7 @@
+package com.navigation
+
+const val ROUT_HOME = "home"
+const val ROUT_ABOUT = "about"
+const val ROUT_ITEM = "item"
+const val ROUT_START = "start"
+
---
+++ app/src/main/java/com/nyokabi/harakamall/ui/screens/Home/HomeScreen.kt
@@ -0,0 +1,80 @@
+package com.nyokabi.harakamall.ui.screens.Home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.navigation.ROUT_START
+import com.nyokabi.harakamall.R
+
+@Composable
+fun HomeScreen(navController: NavController){
+
+Column (
+    modifier = Modifier.fillMaxSize()
+){
+    Text(
+        text = "HarakaMall",
+        fontSize = 30.sp,
+        color = Color.Magenta,
+        fontWeight = FontWeight.ExtraBold
+
+    )
+
+    Image(
+        painter = painterResource(R.drawable.home),
+        contentDescription = "home"
+    )
+
+
+
+
+    Text(
+        text = "Welcome to my app",
+        fontSize = 15.sp,
+        fontStyle = FontStyle.Italic
+    )
+
+    Text(
+        text = "welcome to my mall where all services are important and available with an affordable budget",
+        fontSize = 15.sp,
+        fontStyle = FontStyle.Italic
+    )
+
+    Button(onClick = {
+        navController.navigate(ROUT_START)
+    },
+        colors = ButtonDefaults.buttonColors(),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Text( text = "Add to cart")
+    }
+
+}
+
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+
+HomeScreen(rememberNavController())
+
+}
